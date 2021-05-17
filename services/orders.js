5fb7e73f6e0f40bcfffde7c25cea66fff444ed72
DavidCZ94 <david940103@gmail.com>
2021-05-17 18:33:49 -0500

    order creation actions

--- services/orders.js
+++ services/orders.js
@@ -6,6 +6,17 @@ class OrdersService {
         this.mongoDB = new MongoLib();
     }
 
+    async getOrders( { search }){
+        const query = {
+            $or: [
+                {clientId:  new RegExp(`.*${search}.*` ,`i`)},
+                {paid_out:  new RegExp(`.*${search}.*` ,`i`)},
+                {status:  new RegExp(`.*${search}.*` ,`i`)},
+                {creation_date:  new RegExp(`.*${search}.*` ,`i`)},
+            ]
+        }
+    }
+
     async createOrder( { order } ){
         const createdOrderId = await this.mongoDB.create(
             this.collection, order );
